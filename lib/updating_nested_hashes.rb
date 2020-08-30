0e055e985980f8a81110a4cd646c047cbdcbf292
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-30 19:41:21 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,7 +1,4 @@
 def update_status
-  epic_tragedy[:montague][:hero][:status] = "dead"
-
-  puts epic_tragedy
    epic_tragedy = {
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
